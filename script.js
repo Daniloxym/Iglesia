08383320cbd0da32ea6585fe488578d5f7af47c1
Danilo Olivos <daniolip2021@gmail.com>
2025-03-03 12:25:39 -0500

    Mejorando

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const menuOpenButton = document.querySelector(".navegacion__cerrar");
+
+menuOpenButton.addEventListener("click",()=>{
+
+    document.body.classList.toggle("show-mobile-menu");
+
+
+
+})

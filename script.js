551bf8986a3546bd6dde7421324360f005622c1a
Danilo Olivos <daniolip2021@gmail.com>
2025-03-05 17:37:39 -0500

    Mejoras con respecto al contraste de colores y filtro de blur agregado

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const menuOpenButton = document.querySelector(".navegacion__bars");
 menuCloseButton.addEventListener("click", () => {
 
   document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("efecto");
 });
 
 menuOpenButton.addEventListener("click",()=>{
 
     document.body.classList.toggle("show-mobile-menu");
-
+    document.body.classList.toggle("efecto");
 
 })
 

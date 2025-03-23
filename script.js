0f02d90a9c1917b795b57f3c67f5656e9b2f2d31
Danilo Olivos <daniolip2021@gmail.com>
2025-03-23 17:29:19 -0500

    Guardado

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+//Creacion de botones para abrir y cerrar navbar
 const menuCloseButton = document.querySelector(".navegacion__cerrar");
 const menuOpenButton = document.querySelector(".navegacion__bars");
 
-
 menuCloseButton.addEventListener("click", () => {
-
   document.body.classList.toggle("show-mobile-menu");
   document.body.classList.toggle("efecto");
 });
 
-menuOpenButton.addEventListener("click",()=>{
-
-    document.body.classList.toggle("show-mobile-menu");
-    document.body.classList.toggle("efecto");
-
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("efecto");
 });
+
+//Agregar linea de estado en navbar
+
 
